fe5cda6e5dda7909771059bf98bb43f559334ece
protyliss <protyliss@gmail.com>
2019-05-16 17:23:34 +0900

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+const gulp = require('gulp');
+const pump = require('pump');
+
+const sass = require('gulp-sass');
+const $sass = () => sass().on('error', sass.logError);
+
+const sassFiles = '**/*.scss';
+
+const glamSrc = './glam/';
+const themeSrc = '../../theme/basic/';
+const glamCssSrc = glamSrc + 'css/';
+const themeCssSrc = themeSrc + 'css/';
+
+const glamSassFiles = glamCssSrc + sassFiles;
+const themeSassFiles = themeCssSrc + sassFiles;
+
+gulp.task('glam-sass', done => {
+	pump(
+		gulp.src(glamSassFiles),
+		$sass(),
+		gulp.dest(glamCssSrc),
+		done
+	);
+});
+
+gulp.task('theme-sass', done => {
+	pump(
+		gulp.src(themeSassFiles),
+		$sass(),
+		gulp.dest(themeCssSrc),
+		done
+	);
+});
+
+gulp.task('css-watch', done => {
+	gulp.watch(glamSassFiles, gulp.series('glam-sass'));
+	gulp.watch(themeSassFiles, gulp.series('theme-sass'));
+	done();
+});
+
+gulp.task('css', gulp.series(
+	'glam-sass',
+	'theme-sass',
+	'css-watch'
+));
+
+gulp.task('default', gulp.parallel(
+	'css'
+));
