522e40f65999dfe15f67f701214783af1e49d8ad
protyliss <protyliss@gmail.com>
2019-06-07 14:34:50 +0900

    change stylesheet declare file

--- share/headSub.php
+++ share/headSub.php
@@ -3,18 +3,6 @@
  * @global $glam Glam\GlamBoard
  */
 
-$glam = \Glam\Glam::that();
-$head = &$glam->head;
-
-$head->style(GLAM_CSSDOT . 'reset.css');
-$head->style(GLAM_CSSDOT . 'dynamic/frame/1600.css');
-$head->style(GLAM_CSSDOT . 'dynamic/grid.css');
-$head->style(GLAM_CSSDOT . 'box.css');
-$head->style(GLAM_CSSDOT . 'lift.css');
-$head->style('https://fonts.googleapis.com/css?family=Noto+Sans+KR');
-
-$head->style(GNU_THEME_CSS . 'global.css');
-
 if (!isset($g5['title'])) {
 	$g5['title'] = $config['cf_title'];
 	$g5_head_title = $g5['title'];
